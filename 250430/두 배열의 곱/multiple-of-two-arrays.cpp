e7d1e44f19deb1ebc55f7689421a8b61454810cf
willbezero <hsy6927@naver.com>
2025-04-30 13:12:48 +0900

    Title: 두 배열의 곱, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250430/두 배열의 곱/multiple-of-two-arrays.cpp
+++ 250430/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -3,22 +3,29 @@ using namespace std;
 
 int main() {
     int array[3][3];
-    
+    int array2[3][3];
+    int sum[3][3];
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cin >> array[i][j]l
+            cin >> array[i][j];
          }
      }
      
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            array[i][j] *= 2;
+            cin >> array2[i][j];
         }
      }
      
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cout << array[i][j] << " ";
+           sum[i][j] = array[i][j] * array2[i][j];
+        }
+    } 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << sum[i][j] << " ";
         }
     cout << endl;
      } // Please write your code here.
