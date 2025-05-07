98a9c103c4ac14a8af1ceb8015d0dc67b9fa456e
willbezero <hsy6927@naver.com>
2025-05-07 13:38:21 +0900

    Title: 배열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250507/배열의 합/sum-of-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[4][4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> array[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        int sum = 0;
+        for (int j = 0; j < 4; j++) {
+        sum += array[i][j];
+        }
+        cout << array[i][j] << endl;
+    }// Please write your code here.
+    return 0;
+}
