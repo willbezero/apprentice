09becfc24ebe7cd6995692a618f4a9f5cd2d9d90
willbezero <hsy6927@naver.com>
2025-05-08 13:20:31 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250508/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
+
+    if (A > B) cout << "1" << endl;
+    else cout << "0" << endl;
+    if (A > C) cout << "1" << endl;
+    else cout << "0" << endl;
+    if (A > B) cout << "1" << endl;
+    else cout << "0" << endl;
+    return 0;
+}
