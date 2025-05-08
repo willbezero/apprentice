aec18cb4a0ab542049ac27901affbd7b43fba8fe
willbezero <hsy6927@naver.com>
2025-05-08 13:13:19 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250508/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a >= 3000) cout << "book";
+    else if (a >= 1000) cout << "mask";
+    else if (a >= 500) cout << "pen";
+    else cout << "no";
+    return 0;
+}
