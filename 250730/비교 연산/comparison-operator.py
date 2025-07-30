5052dc2af36ec0c57c354aad99321da8981fa7f5
willbezero <hsy6927@naver.com>
2025-07-30 19:01:00 +0900

    Title: 비교 연산, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250730/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
